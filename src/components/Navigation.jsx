ab4e669b563371bd7f6fdb744759c473bef9f3b5
bokang-thiago05 <belengbokangmalebo@gmail.com>
2026-01-09 09:28:38 +0200

    Replace Vite logo with BBM branding

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../assets/logo.svg';
+import logo from '../assets/favicon.png';
 
 const Navigation = ({ activeSection }) => {
   const scrollToSection = (id) => {
